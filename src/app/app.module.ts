f34d01e722d8c10c12faae43a70707aed1fbdd2d
harryhuy1 <intern.ndhuy@cmc.com.vn>
2019-07-05 08:50:45 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AngularDualListBoxModule } from 'angular-dual-listbox';
+
+import { DemoAppComponent } from './demo-app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+	imports:      [ BrowserModule, FormsModule, AngularDualListBoxModule ],
+	declarations: [ DemoAppComponent ],
+	bootstrap:    [ DemoAppComponent ]
 })
 export class AppModule { }
+
